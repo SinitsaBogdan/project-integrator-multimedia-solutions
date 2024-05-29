4c8a6090212c8da6ce7ea81032585f3f0d9e4c5d
Platon Ponomarchuk <platonponomarchuk@yandex.ru>
2024-05-29 14:25:19 +0300

    added createMenuElement function

---
+++ scripts/create-menu-element.js
@@ -0,0 +1,15 @@
+function createMenuElements() {
+  const menuList = document.querySelector('.Menu__navigation');
+
+  menuItems.forEach((item) => {
+    const menuListItem = document.createElement('li');
+    const menuItemElment = document.createElement('a');
+
+    menuListItem.classList.add('Navigation__item');
+    menuItemElment.textContent = item.nameEng;
+    menuItemElment.href = item.url;
+
+    menuListItem.append(menuItemElment);
+    menuList.append(menuListItem);
+  });
+}
