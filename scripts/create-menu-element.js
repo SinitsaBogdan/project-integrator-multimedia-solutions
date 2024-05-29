13f64393e011e515ac106fe05b7474a114a45942
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2024-05-29 17:55:53 +0300

    refactor : project

--- scripts/create-menu-element.js
+++ scripts/create-menu-element.js
@@ -1,13 +1,13 @@
 import { menuItems } from "./menu-data.js";
 
 export function createMenuElements() {
-	const menuList = document.querySelector('.Menu__navigation');
+	const menuList = document.querySelector('.Menu__list');
 
 	menuItems.forEach((item) => {
 		const menuListItem = document.createElement('li');
 		const menuItemElment = document.createElement('a');
 
-		menuListItem.classList.add('Navigation__item');
+		menuListItem.classList.add('Menu__item');
 		menuItemElment.textContent = item.nameEng;
 		menuItemElment.href = item.url;
 
