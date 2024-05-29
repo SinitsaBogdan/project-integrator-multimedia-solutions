226f73846744388f703c5656cc035d6f427a2870
Platon Ponomarchuk <platonponomarchuk@yandex.ru>
2024-05-29 14:25:45 +0300

    fixed index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,1 @@
-const menuNavigation = document.querySelector('.menu__navigation');
-const navigationItemTemplate = document.querySelector('.navigation__item-template').content;
-
-menuItems.forEach((item) => {
-  const navigationItemElement = navigationItemTemplate.querySelector('.navigation__item').cloneNode(true);
-  const navigationLinkElement = navigationItemElement.querySelector('a');
-  navigationLinkElement.textContent = item.name;
-  navigationLinkElement.href = item.url;
-  menuNavigation.append(navigationItemElement);
-})
+createMenuElements();
