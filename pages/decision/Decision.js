3234faca2ae6361fdb6b971199ba0a31d7c8add2
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2024-06-03 13:56:36 +0300

    Update Decision.js

--- pages/decision/Decision.js
+++ pages/decision/Decision.js
@@ -1,5 +1,6 @@
 import { createMenuElements } from '../../scripts/create-menu-element.js';
 import { createStageElements } from '../../scripts/create-stages.js';
+import { stageElementsDecision as elements } from '../../data/data-stage.js';
 
 createMenuElements();
-createStageElements();
+createStageElements(elements);
