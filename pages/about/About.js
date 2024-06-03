5240fe0b448697da572fb00f447c1f3ef2237991
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2024-06-03 16:05:30 +0300

    update : page About

--- pages/about/About.js
+++ pages/about/About.js
@@ -1,3 +1,6 @@
 import { createMenuElements } from '../../scripts/create-menu-element.js';
+import { createStageElements } from '../../scripts/create-stages.js';
+import { stageElementsAdvantages as advantages } from '../../data/data-stage.js';
 
 createMenuElements();
+createStageElements("advantages", advantages);
