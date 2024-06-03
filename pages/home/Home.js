617ffc58aa458585ab4d5e2b112e4b0570183b8c
Platon <ponomarchukdevelop@gmail.com>
2024-06-04 00:48:07 +0300

    Create : license (#22)
    * refactor : project
    
    Устранение некоторых расхождений в общей стилистике кода.
    Приведение к общим блокам и стилям.
    Оптимизация ссылок ( переходов ) от страницы к странице.
    
    * add : Licenses block
    
    * add : Licenses styles
    
    * add : script creating licenses
    
    * update : home.html
    
    * delete : Header__button.css
    
    Лишний класс и стили, которые не используются
    
    * refactor : project
    
    Изменил немного стили для Wrapper, padding-inline только в header используется
    
    * update : header.css
    
    * add : Licenses__wrapper
    
    * fix : home.html
    
    Исправил внешний вид кнопки в диалоговом окне
    
    * update : About
    
    Добавлен блок лицензий.
    
    ---------
    
    Co-authored-by: Синица Богдан <sinitsa.bogdan.1997@yandex.ru>

--- pages/home/Home.js
+++ pages/home/Home.js
@@ -1,5 +1,6 @@
 import { createMenuElements } from '../../scripts/create-menu-element.js';
 import { vievSlidersFirstscrean } from '../../scripts/create-sliders-firstscrean.js';
+import { addLicenses } from '../../scripts/create-license-list.js';
 
 import { createStageElements } from '../../scripts/create-stages.js';
 import { stageElementsAdvantages as advantages } from '../../data/data-stage.js';
@@ -7,3 +8,4 @@ import { stageElementsAdvantages as advantages } from '../../data/data-stage.js'
 createMenuElements();
 vievSlidersFirstscrean();
 createStageElements("advantages", advantages);
+addLicenses();
