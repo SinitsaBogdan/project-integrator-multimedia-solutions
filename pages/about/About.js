3a03eebe9bf5533c68f86147d68dd7fbd739c734
Platon <ponomarchukdevelop@gmail.com>
2024-06-04 00:38:23 +0300

    update : About
    Добавлен блок лицензий.

--- pages/about/About.js
+++ pages/about/About.js
@@ -1,3 +1,5 @@
 import { createMenuElements } from '../../scripts/create-menu-element.js';
+import { addLicenses } from '../../scripts/create-license-list.js';
 
 createMenuElements();
+addLicenses();
