e2dea47ee6959c221bdc65886c0dc723e118f965
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2024-06-03 13:56:42 +0300

    Update Home.js

--- pages/home/Home.js
+++ pages/home/Home.js
@@ -1,5 +1,9 @@
 import { createMenuElements } from '../../scripts/create-menu-element.js';
 import { vievSlidersFirstscrean } from '../../scripts/create-sliders-firstscrean.js';
 
+import { createStageElements } from '../../scripts/create-stages.js';
+import { stageElementsHome as elements } from '../../data/data-stage.js';
+
 createMenuElements();
 vievSlidersFirstscrean();
+createStageElements(elements);
