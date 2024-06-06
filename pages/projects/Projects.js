df001a793f2a0844ca9a0f1848d5115eaa245c2c
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2024-06-06 17:58:28 +0300

    Update Projects.js

--- pages/projects/Projects.js
+++ pages/projects/Projects.js
@@ -1,3 +1,6 @@
 import { createMenuElements } from '../../scripts/create-menu-element.js';
+import { dataElementsProjects as list } from '../../data/data-projects.js';
+import { createElementsCard } from '../../scripts/create-elements-card.js';
 
 createMenuElements();
+createElementsCard('ListProjects', list);
