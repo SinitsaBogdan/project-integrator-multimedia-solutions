e448c61cc3fed92499610e399d5a785564a73c26
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2024-06-06 15:19:09 +0300

    Update Sectors.js

--- pages/sectors/Sectors.js
+++ pages/sectors/Sectors.js
@@ -1,3 +1,6 @@
 import { createMenuElements } from '../../scripts/create-menu-element.js';
+import { dataElementsIndustries as list } from '../../data/data-industries.js';
+import { createElementsIndustries } from '../../scripts/create-elements-industries.js';
 
 createMenuElements();
+createElementsIndustries(list);
