6025d0ee02b97a7594b7e3e5deb3a5da47247e55
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2024-06-17 18:30:22 +0300

    Update Decision.js

--- pages/decision/Decision.js
+++ pages/decision/Decision.js
@@ -1,10 +1,19 @@
-import { createMenuElements } from '../../scripts/create-menu-element.js';
-import { createStageElements } from '../../scripts/create-stages.js';
-import { stageElementsAdvantages as advantages } from '../../data/data-stage.js';
-import { stageElementsIntegration as integration } from '../../data/data-stage.js';
-import { createClients } from '../../scripts/create-clients-list.js';
+// import { createMenuElements } from '../../scripts/create-menu-element.js';
+// import { createStageElements } from '../../scripts/create-stages.js';
+// import { stageElementsAdvantages as advantages } from '../../data/data-stage.js';
+// import { stageElementsIntegration as integration } from '../../data/data-stage.js';
+// import { createClients } from '../../scripts/create-clients-list.js';
 
-createMenuElements();
-createStageElements("advantages", advantages);
-createStageElements("integration", integration);
-createClients();
+// createMenuElements();
+// createStageElements("advantages", advantages);
+// createStageElements("integration", integration);
+// createClients();
+
+// ----------------------------------------------------------------------
+
+import * as project from '../../scripts/general-project.js'
+
+project.createNavigationsHeader()
+project.createNavigationsFooter()
+project.createNavigationsDecisions()
+project.createNavigationsIndustries()
