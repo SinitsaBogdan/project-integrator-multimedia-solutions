6c7af327c8beb2e4869cc7438390e403799552a0
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2024-06-17 18:30:16 +0300

    Update Project.js

--- pages/project/Project.js
+++ pages/project/Project.js
@@ -1,6 +1,15 @@
-import { createMenuElements } from '../../scripts/create-menu-element.js';
-import { searchSimilarProject } from '../../scripts/search-similar-project.js';
-import { dataElementsProjects as list } from '../../data/data-projects.js';
+// import { createMenuElements } from '../../scripts/create-menu-element.js';
+// import { searchSimilarProject } from '../../scripts/search-similar-project.js';
+// import { dataElementsProjects as list } from '../../data/data-projects.js';
 
-createMenuElements();
-searchSimilarProject(2, list);
+// createMenuElements();
+// searchSimilarProject(2, list);
+
+// ----------------------------------------------------------------------
+
+import * as project from '../../scripts/general-project.js'
+
+project.createNavigationsHeader()
+project.createNavigationsFooter()
+project.createNavigationsDecisions()
+project.createNavigationsIndustries()
