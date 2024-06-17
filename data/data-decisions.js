996bbb72cb18ee6443e4ad7844067de35d9fcf8a
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2024-06-17 18:30:41 +0300

    Create data-decisions.js

---
+++ data/data-decisions.js
@@ -0,0 +1,80 @@
+export const dataDecisionsElements = [
+	{
+		nameRu: 'Ситуационный центр',
+		nameEng: '',
+		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+		image: '../../assets/image/Industries/item-01.jpg',
+		href: '../decision/Decision.html',
+	},
+	{
+		nameRu: 'Диспетчерский пункт',
+		nameEng: '',
+		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+		image: '../../assets/image/Industries/item-01.jpg',
+		href: '../decision/Decision.html',
+	},	
+	{
+		nameRu: 'Зал совещаний',
+		nameEng: '',
+		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+		image: '../../assets/image/Industries/item-01.jpg',
+		href: '../decision/Decision.html',
+	},
+	
+	{
+		nameRu: 'Конференц зал',
+		nameEng: '',
+		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+		image: '../../assets/image/Industries/item-01.jpg',
+		href: '../decision/Decision.html',
+	},
+	
+	{
+		nameRu: 'Переговорная комната',
+		nameEng: '',
+		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+		image: '../../assets/image/Industries/item-01.jpg',
+		href: '../decision/Decision.html',
+	},
+	
+	{
+		nameRu: 'Видеоконференцсвязь',
+		nameEng: '',
+		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+		image: '../../assets/image/Industries/item-01.jpg',
+		href: '../decision/Decision.html',
+	},
+	
+	{
+		nameRu: 'Кабинет руководителя',
+		nameEng: '',
+		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+		image: '../../assets/image/Industries/item-01.jpg',
+		href: '../decision/Decision.html',
+	},
+	
+	{
+		nameRu: 'Digital Signage',
+		nameEng: '',
+		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+		image: '../../assets/image/Industries/item-01.jpg',
+		href: '../decision/Decision.html',
+	},
+	
+	{
+		nameRu: 'Медиафасады',
+		nameEng: '',
+		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+		image: '../../assets/image/Industries/item-01.jpg',
+		href: '../decision/Decision.html',
+	},
+	
+	{
+		nameRu: 'Учебные аудитории',
+		nameEng: '',
+		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+		image: '../../assets/image/Industries/item-01.jpg',
+		href: '../decision/Decision.html',
+	},
+	
+];
