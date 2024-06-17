ee419231eb125e9f08a3d5cfc19718bf893479bf
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2024-06-17 18:30:13 +0300

    Update Sectors.js

--- pages/sectors/Sectors.js
+++ pages/sectors/Sectors.js
@@ -1,6 +1,15 @@
-import { createMenuElements } from '../../scripts/create-menu-element.js';
-import { dataElementsIndustries as list } from '../../data/data-industries.js';
-import { createElementsCard } from '../../scripts/create-elements-card.js';
+// import { createMenuElements } from '../../scripts/create-menu-element.js';
+// import { dataElementsIndustries as list } from '../../data/data-industries.js';
+// import { createElementsCard } from '../../scripts/create-elements-card.js';
 
-createMenuElements();
-createElementsCard('List', list);
+// createMenuElements();
+// createElementsCard('List', list);
+
+// ----------------------------------------------------------------------
+
+import * as project from '../../scripts/general-project.js'
+
+project.createNavigationsHeader()
+project.createNavigationsFooter()
+project.createNavigationsDecisions()
+project.createNavigationsIndustries()
