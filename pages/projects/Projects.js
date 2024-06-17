c392ecf755e50c8ccfbeea7fac9d577bead57706
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2024-06-17 18:30:15 +0300

    Update Projects.js

--- pages/projects/Projects.js
+++ pages/projects/Projects.js
@@ -1,6 +1,15 @@
-import { createMenuElements } from '../../scripts/create-menu-element.js';
-import { dataElementsProjects as list } from '../../data/data-projects.js';
-import { createElementsCard } from '../../scripts/create-elements-card.js';
+// import { createMenuElements } from '../../scripts/create-menu-element.js';
+// import { dataElementsProjects as list } from '../../data/data-projects.js';
+// import { createElementsCard } from '../../scripts/create-elements-card.js';
 
-createMenuElements();
-createElementsCard('List', list);
+// createMenuElements();
+// createElementsCard('List', list);
+
+// ----------------------------------------------------------------------
+
+import * as project from '../../scripts/general-project.js'
+
+project.createNavigationsHeader()
+project.createNavigationsFooter()
+project.createNavigationsDecisions()
+project.createNavigationsIndustries()
