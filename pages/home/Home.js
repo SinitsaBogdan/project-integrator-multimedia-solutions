f71e7ef21b717aba5680457aa94705798f3e8ac6
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2024-06-17 12:14:31 +0300

    refactor : block ForWhom

--- pages/home/Home.js
+++ pages/home/Home.js
@@ -1,15 +1,18 @@
+import { stageElementsAdvantages as advantages } from '../../data/data-stage.js';
+import { tagElements as tags } from '../../data/data-tags.js';
+
 import { createMenuElements } from '../../scripts/create-menu-element.js';
 import { vievSlidersFirstscrean } from '../../scripts/create-sliders-firstscrean.js';
 import { addLicenses } from '../../scripts/create-license-list.js';
-
 import { createStageElements } from '../../scripts/create-stages.js';
-import { stageElementsAdvantages as advantages } from '../../data/data-stage.js';
 import { createStageSolutionElements } from '../../scripts/create-stages-solutions.js';
 import { createClients } from '../../scripts/create-clients-list.js';
+import { createTagElements } from '../../scripts/create-tags.js';
 
 createMenuElements();
 vievSlidersFirstscrean();
-createStageElements("advantages", advantages);
+createStageElements('advantages', advantages);
 addLicenses();
 createStageSolutionElements();
 createClients();
+createTagElements('ForWhom__list', tags);
