b14f300d5c4c685750edbc11aa730abdba73d2de
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2024-06-17 18:30:19 +0300

    Update Home.js

--- pages/home/Home.js
+++ pages/home/Home.js
@@ -1,18 +1,25 @@
-import { stageElementsAdvantages as advantages } from '../../data/data-stage.js';
-import { tagElements as tags } from '../../data/data-tags.js';
+// import { stageElementsAdvantages as advantages } from '../../data/data-stage.js';
+// import { tagElements as tags } from '../../data/data-tags.js';
 
-import { createMenuElements } from '../../scripts/create-menu-element.js';
-import { vievSlidersFirstscrean } from '../../scripts/create-sliders-firstscrean.js';
-import { addLicenses } from '../../scripts/create-license-list.js';
-import { createStageElements } from '../../scripts/create-stages.js';
-import { createStageSolutionElements } from '../../scripts/create-stages-solutions.js';
-import { createClients } from '../../scripts/create-clients-list.js';
-import { createTagElements } from '../../scripts/create-tags.js';
+// import { createMenuElements } from '../../scripts/create-menu-element.js';
+// import { vievSlidersFirstscrean } from '../../scripts/create-sliders-firstscrean.js';
+// import { addLicenses } from '../../scripts/create-license-list.js';
+// import { createStageElements } from '../../scripts/create-stages.js';
+// import { createClients } from '../../scripts/create-clients-list.js';
+// import { createTagElements } from '../../scripts/create-tags.js';
 
-createMenuElements();
-vievSlidersFirstscrean();
-createStageElements('advantages', advantages);
-addLicenses();
-createStageSolutionElements();
-createClients();
-createTagElements('ForWhom__list', tags);
+// createMenuElements();
+// vievSlidersFirstscrean();
+// createStageElements('advantages', advantages);
+// addLicenses();
+// createClients();
+// createTagElements('ForWhom__list', tags);
+
+// ----------------------------------------------------------------------
+
+import * as project from '../../scripts/general-project.js'
+
+project.createNavigationsHeader()
+project.createNavigationsFooter()
+project.createNavigationsDecisions()
+project.createNavigationsIndustries()
